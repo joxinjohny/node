2c5e2e80b416d1cbb1ebbcc22eb0ae61abda1f40
Joxin Johny (Industrial JO854817 Engineering Services (IJO854817ES)) <JO854817@wipro.com>
2020-08-13 22:35:15 +0530

    Learned Yargs custom commands

--- npm/user-input.js
+++ npm/user-input.js
@@ -1,6 +1,6 @@
-const fs = require('fs')
-
-
+const chalk = require('chalk');
+const yargs = require('yargs');
+/* 
 const command = process.argv[2]
 
 if(command === 'add') {
@@ -8,3 +8,35 @@ if(command === 'add') {
 } else if(command === 'remove'){
     console.log('Removing Note...')
 }
+ */
+yargs.version('1.1.0')
+
+yargs.command({
+        command: 'add',
+        describe: 'Add a note',
+        builder: {
+            title: {
+                describe: 'Title of the note',
+                demandOption: true,
+                type: 'string'
+            },
+            body: {
+                describe: 'Body of the note',
+                demandOption: true,
+                type: 'string'
+            }
+        },
+        handler: function(argv) {
+            console.log('Adding Note with title ' + chalk.green.inverse(argv.title) + ' and contents ' + chalk.blue.inverse(argv.body))
+        }
+    })
+
+yargs.command({
+        command: 'remove',
+        describe: 'Remove a note',
+        handler: function() {
+            console.log('Removing Note...')
+        }
+    })
+
+yargs.parse()
